a2fe7381c3a8705cc2d8f304bdf53a945c1a095d
leena0203 <leenasarode02@gmail.com>
2020-10-18 20:38:01 +0530

    Created new method to add elements in binary search tree

--- src/test/java/com/MyBinaryTree/MyBinaryTreeTest.java
+++ src/test/java/com/MyBinaryTree/MyBinaryTreeTest.java
@@ -18,5 +18,27 @@ class MyBinaryTreeTest {
 		test.add(70);
 		assertEquals(3, test.getSize());
 	}
+	
+	/**
+	 * Create binary tree 
+	 */
+	@Test
+	public void given13Numbers_whenAddedToBinaaryTree_shouldReturnSize() {
+		MyBinaryTree<Integer> test = new MyBinaryTree<>();
+		test.add(56);
+		test.add(30);
+		test.add(70);
+		test.add(22);
+		test.add(60);
+		test.add(40);
+		test.add(11);
+		test.add(3);
+		test.add(95);
+		test.add(65);
+		test.add(63);
+		test.add(16);
+		test.add(67);
+		assertEquals(13, test.getSize());
+	}
 
 }
